27fc4c08f62defe806ab1c6005d9e96af754290f
gise ares <gise.ares@gmail.com>
2021-11-22 18:12:27 -0300

    views, shows, routes and cases

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -1,4 +1,10 @@
 class DirectionsController < ApplicationController
+
+  def index
+    @directions = Direction.all
+  end
+
   def show
+    @direction = Direction.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_22_205118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "directions", force: :cascade do |t|
+    t.string "street"
+    t.integer "outside_number"
+    t.integer "inside_number"
+    t.string "cp"
+    t.string "colonia"
+    t.string "delegation"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
